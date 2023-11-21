d8386457bd1ee6e3accaeaf5b2318383919b1830
Bharath Devarasetty <Bharath>
2023-11-21 21:32:10 +0530

    first commit

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Bharath Devarasetty",
+  },
+  {
+    label: "Date of Birth",
+    value: "25th Dec 2000",
+  },
+  {
+    label: "Address",
+    value: "Hyderabad ,India",
+  },
+  {
+    label: "Email",
+    value: "bharathdevarasetty2@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+91 9705577876",
+  },
+];
+
+const jobSummary =
+  "Motivated and results-driven professional with two years of hands-on experience as a SharePoint Developer and DevOps Engineer. Passionate about creating captivating user interfaces while optimizing software development through efficient deployment and automation. Proficient in SharePoint Online, React JS, and DevOps, Ensuring the seamless delivery of robust and business-aligned applications.";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>SharePoint Developer and DevOps Engineer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,35 @@
+export const data = {
+    experience: [
+      {
+        title: "Project Engineer",
+        subTitle: "WIPRO LIMITED, Hyderabad",
+        description:
+          "SharePoint Developer and DevOps Engineer",
+          date: "July 2021 - Present"
+      },
+      
+    ],
+    education: [
+      {
+        title: "B.Tech",
+        subTitle: "Amrita Vishwa Vidyapeetham, Bengaluru, karnataka",
+        description:
+          "Electronics and communication  Engineering",
+          date: "JUNE 2017 – JUNE 2021"
+      },
+      {
+        title: "Intermediate",
+        subTitle: "Abhyaas Junior College, Guntur, AP",
+        description:
+          "M.P.C",
+          date: "JUNE 2015 – JUNE 2017"
+      },
+      {
+        title: "High School",
+        subTitle: "Sri Chaitanya Techno School, Guntur, AP",
+        description:
+          "State Board",
+          date: "JUNE2014 - JUNE2015"
+      },
+    ],
+  };
